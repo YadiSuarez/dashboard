b326e8c04102768f845ea301043bb16e4b832202
YadiSuarez <151183232+YadiSuarez@users.noreply.github.com>
2025-06-25 09:51:29 -0500

    guia 13

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,8 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import {Grid} from '@mui/material'
-import './App.css'
+import { useState } from 'react';
+import HeaderUI from './components/headerUI';
+import AlertUI from './components/AlertUI';
+import {Grid} from '@mui/material';
+import './App.css';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,10 +11,14 @@ function App() {
     <Grid container spacing={5} justifyContent="center" alignItems="center">
 
          {/* Encabezado */}
-         <Grid size={{ xs: 12, md: 12 }}>Elemento: Encabezado</Grid>
+         <Grid size={{ xs: 12, md: 12 }}>
+          <HeaderUI/>
+         </Grid>
 
          {/* Alertas */}
-         <Grid size={{ xs: 12, md: 12 }}>Elemento: Alertas</Grid>
+         <Grid size={{ xs: 12, md: 12 }} container justifyContent="right" alignItems="center">
+          <AlertUI description='No se preveen lluvias'/>
+         </Grid>
 
          {/* Selector */}
          <Grid size={{ xs: 12, md: 3 }}>Elemento: Selector</Grid>
