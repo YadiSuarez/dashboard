63922c617e69eee9018c1042f8dc149e14d4d189
YadiSuarez <151183232+YadiSuarez@users.noreply.github.com>
2025-07-22 23:33:48 -0500

    componente no necesario

--- vite-project/dev-dist/sw.js
+++ vite-project/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-e755d862'], (function (workbox) { 'use strict';
     "revision": "21199adf659a9b979572e74b6e36502f"
   }, {
     "url": "index.html",
-    "revision": "0.abt01imr0ig"
+    "revision": "0.ks6vt95u88o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
